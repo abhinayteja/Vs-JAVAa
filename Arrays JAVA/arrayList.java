7b0bdd8bdbdc620e1918dd2f3347c049af69e5d6
abhinayteja <abhinayteja885@gmail.com>
2025-12-05 16:06:15 +0530

    Array list

--- Arrays JAVA/arrayList.java
+++ Arrays JAVA/arrayList.java
@@ -1,8 +1,20 @@
+import java.util.ArrayList;
 
-// import java.lang.reflect.Array;
+public class arrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(20);
+        arr.add(30);
+        arr.add(20);
+        arr.add(30);
+        arr.add(20);
+        arr.add(30);
+        arr.add(20);
+        arr.add(30);
 
-// public class arrayList {
-//     public static void main(String[] args) {
         
-//     }
-// }
+        System.out.println(arr);
+        System.out.println(arr.get(4));
+    }
+    
+}
